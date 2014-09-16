f220ca3e5f047e2273a60a359e73a124c2d5f6d0
sureshnarayanan <sureshnarayanan@users.noreply.github.com>
2014-09-15 17:14:46 -0700

    Added Utilities Project

---
+++ Utilities/GenerateSecureTimeToken.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    class GenerateSecureTimeToken
+    {
+        public void GenerateToken()
+        {
+            int tokenExpirationtime = 5; //in minutes
+            //Generate the token for given time using random guid
+            byte[] time = BitConverter.GetBytes(DateTime.UtcNow.ToBinary());
+            byte[] key = Guid.NewGuid().ToByteArray();
+            string token = Convert.ToBase64String(time.Concat(key).ToArray());
+            Console.WriteLine("This will be the token that should be passed in URL: " + token);
+           
+
+            byte[] data = Convert.FromBase64String(token);
+            DateTime when = DateTime.FromBinary(BitConverter.ToInt64(data, 0));
+            if (when < DateTime.UtcNow.AddMinutes(-tokenExpirationtime))
+            {
+                Console.WriteLine("Token Expired.");
+            }
+            else
+            {
+                Console.WriteLine("Valid token within time limit.");                
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ Utilities/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Generate time based token
+           new GenerateSecureTimeToken().GenerateToken();
+        }
+    }
+}
